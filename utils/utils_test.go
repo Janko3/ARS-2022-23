8ccc6845343b01aac0b5c677b496baf98da4832d
XenZi <rilakmateja@gmail.com>
2023-06-06 20:54:02 +0200

    Basic utils test implementation

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -48,3 +48,46 @@ func TestDecodeBodyGroup(t *testing.T) {
 		t.Errorf("Test not successful. Expected: ConfigurationGroup, but got: %s", err.Error())
 	}
 }
+
+func TestGetKeyIndexInfo(t *testing.T) {
+	key := "config/groupID/groupVersion/someValue"
+
+	valueLookingFor := "groupID"
+	expected := "groupID"
+	result := GetKeyIndexInfo(valueLookingFor, key)
+
+	if result != expected {
+		t.Errorf("Expected '%s', but got '%s'", expected, result)
+	}
+
+	valueLookingFor = "groupVersion"
+	expected = "groupVersion"
+	result = GetKeyIndexInfo(valueLookingFor, key)
+
+	if result != expected {
+		t.Errorf("Expected '%s', but got '%s'", expected, result)
+	}
+
+	valueLookingFor = "someValue"
+	expected = ""
+	result = GetKeyIndexInfo(valueLookingFor, key)
+
+	if result != expected {
+		t.Errorf("Expected '%s', but got '%s'", expected, result)
+	}
+}
+
+func TestGetLabelAsStringWithSeparator(t *testing.T) {
+	label := map[string]string{
+		"key1": "value1",
+		"key2": "value2",
+		"key3": "value3",
+	}
+
+	expected := "key1:value1;key2:value2;key3:value3"
+	result := GetLabelAsStringWithSeparator(label)
+
+	if result != expected {
+		t.Errorf("Expected '%s', but got '%s'", expected, result)
+	}
+}
